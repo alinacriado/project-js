0caef721ffd0d6d71a5bd07a3507a9a43f1f74b8
Alina <bakhvalovaalina1@gmail.com>
2022-07-07 18:55:59 +0300

    add a value to the period variable

--- project.js
+++ project.js
@@ -3,6 +3,6 @@ const income = 'taxi, shop, cafe';
 const addExpenses = 'dog, car, games, sweet';
 const deposit = true;
 const mission = 164387;
-let period;
+const period = 8;
 alert("hello");
 console.log(233);
