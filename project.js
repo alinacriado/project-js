872731754bbc191491141320edc2c4d3ac28b8bc
Alina <bakhvalovaalina1@gmail.com>
2022-07-07 19:42:14 +0300

    add the budgetDay variable, output various values to the console

--- project.js
+++ project.js
@@ -4,5 +4,17 @@ const addExpenses = 'dog, car, games, sweet';
 const deposit = true;
 const mission = 164387;
 const period = 8;
-alert("hello");
+const budgetDay = money / 30;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(income.length);
+console.log('Период ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(`период ${period} месяцев`);
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay, money % 30);
+
+alert('hello');
 console.log(233);
