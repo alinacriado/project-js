38f996d1696a0bae341c948d80e2dea128e5cca9
Alina <bakhvalovaalina1@gmail.com>
2022-07-08 17:10:33 +0300

    fix: remove spread

--- lesson02-hard/les02-hard.js
+++ lesson02-hard/les02-hard.js
@@ -1,8 +1,7 @@
 const num = 266219;
 const result = [...num.toString()].reduce((a, b) => a * b);
 console.log(result);
-
-console.log(...(result ** 3).toString().substring(0,2));
+console.log((result ** 3).toString().substring(0, 2));
 
 // Старый способ //
 // const newNumb = num.toString().split('');
