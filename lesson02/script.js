60758c40334dda0370e63c4ef88d199ddc25e26f
Alina <bakhvalovaalina1@gmail.com>
2022-07-14 15:13:22 +0300

    dellete consol.log add functions

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,33 +1,18 @@
 'use strict';
 
-const money = +prompt('Ваш месячный доход?');
+const money = +prompt('Ваш месячный доход?', 50000);
 const income = 'taxi, shop, cafe';
-const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'car, sweet, sport');
 const deposit = confirm('Есть ли у вас депозит в банке?');
 const mission = 164387;
 const period = 8;
-const mandatoryExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-const mandatoryExpensesCost1 = +prompt('Во сколько это обойдется?');
-const mandatoryExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-const mandatoryExpensesCost2 = +prompt('Во сколько это обойдется?');
+const mandatoryExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'food');
+const mandatoryExpensesCost1 = +prompt('Во сколько это обойдется?', 10000);
+const mandatoryExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'flat');
+const mandatoryExpensesCost2 = +prompt('Во сколько это обойдется?', 5000);
 const budgetMonth = money - mandatoryExpensesCost1 - mandatoryExpensesCost2;
 const timeForMission = mission / budgetMonth;
 const budgetDay = budgetMonth / 30;
-
-
-
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
-console.log(deposit);
-console.log(income.length);
-console.log('Цель заработать ' + mission + ' рублей');
-console.log(`период ${period} месяцев`);
-console.log(addExpenses.toLowerCase().split(', '));
-console.log(budgetDay, money % 30);
-console.log(budgetMonth);
-console.log(Math.ceil(timeForMission));
-console.log(Math.floor(budgetDay));
 
 if (budgetDay >= 800) {
   console.log('Высокий уровень дохода');
@@ -39,3 +24,18 @@ if (budgetDay >= 800) {
   console.log('Что-то пошло не так');
 }
 
+function getExpensesMonth(expense1,expense2) {
+  return expense1 + expense2;
+}
+
+function getAccumulatedMonth(income,expenses) {
+  return income-expenses;
+}
+const accumulatedMonth = getAccumulatedMonth(money,getExpensesMonth(mandatoryExpensesCost1, mandatoryExpensesCost2));
+
+function getTargetMonth(purpose,month) {
+  return purpose / month;
+}
+
+console.log('Накопления за месяц: ', accumulatedMonth);
+console.log('Срок достижения цели в месяцах: ', Math.floor(getTargetMonth(mission,accumulatedMonth)));
